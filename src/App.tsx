879fabe514fd83fdee42eb33798d1824cbc68c2a
Mia Larkin <mia.larkin1@icloud.com>
2024-07-04 16:42:32 -0400

    landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import NavBarH from './components/navBarH.tsx'
 import NavBarF from './components/navBarF.tsx'
-import bg from './assets/bg.svg'
 import TypeWriter from './components/typeWritter.tsx'
 
 function App() {
@@ -11,14 +10,13 @@ function App() {
       <header>
           <NavBarH></NavBarH>
         </header>
-      <div>
-        <img src={bg} alt="Background" width="100%"/>
-      </div>
-      <div className="computer">
-        <div className="typeWriteDiv">
-          <TypeWriter></TypeWriter>
+      
+        <div className="computer">
+          <div className="typeWriteDiv">
+            <TypeWriter></TypeWriter>
+          </div>
         </div>
-      </div>
+      
     
       <footer>
         <NavBarF></NavBarF>
