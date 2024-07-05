de6347de3078478e32c4f6f2dfb9a30a2a07a1d1
Mia Larkin <mia.larkin1@icloud.com>
2024-07-05 18:25:24 -0400

    slider almost done

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,68 +1,51 @@
-import {
-    RxCrop,
-    RxDesktop,
-    RxPencil2,
-    RxReader,
-    RxRocket,
-    RxAccessibility,
-  } from "react-icons/rx";
-  
-  import c from "./skills/c.png";
-  import css from "./skills/css.png";
-  import html from "./skills/html.png";
-  import java from "./skills/java.png";
-  import oop from "./skills/oop.png";
-  import python from "./skills/python.svg";
-  import react from "./skills/react.png"
-  import riscv from "./skills/riscv.png"
+import c from "./skills/c.png";
+import css from "./skills/css.png";
+import html from "./skills/html.png";
+import java from "./skills/java.png";
+import oop from "./skills/oop.png";
+import python from "./skills/python.svg";
+import react from "./skills/react.png"
+import riscv from "./skills/riscv.png"
   
   export const ServiceData = [
     {
-      icon: RxCrop,
       title: "C",
       content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
-      backgroundImage: c,
+      Image: c,
     },
     {
-      icon: RxPencil2,
       title: "CSS",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: css,
+      Image: css,
     },
     {
-      icon: RxDesktop,
       title: "HTML",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: html,
+      Image: html,
     },
     {
-      icon: RxReader,
       title: "Java",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: java,
+      Image: java,
     },
     {
-      icon: RxAccessibility,
       title: "Object Oriented Programming",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: oop,
+      Image: oop,
     },
     {
-      icon: RxRocket,
       title: "Python",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: python,
+      Image: python,
     },
     {
-      icon: RxRocket,
       title: "React",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: react,
+      Image: react,
     },
     {
-      icon: RxRocket,
       title: "Risc-V",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      backgroundImage: riscv,
+      Image: riscv,
     },
   ];
--- src/components/skillsSlider.tsx
+++ src/components/skillsSlider.tsx
@@ -5,7 +5,6 @@ import 'swiper/css/pagination'
 import 'swiper/css/free-mode'
 
 import { FreeMode, Pagination } from 'swiper/modules'
-import {RxArrowTopRight} from 'react-icons/rx'
 import { ServiceData } from '../assets'
 
 
@@ -34,17 +33,11 @@ const SkillsSwiper = () => {
             {ServiceData.map((item) => (
                 <SwiperSlide key={item.title}>
                     <div className="swiperSlide">
-                        <div
-                        className="images"
-                        style={{ backgroundImage: `url(${item.backgroundImage})` }}
-                        />
-                        <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-                        <div className="relative flex flex-col gap-3">
-                            <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
+                        <div className="card">
+                            <div className="images" style={{ backgroundImage: `url(${item.Image})` }}/>
                             <h1 className="title">{item.title} </h1>
                             <p className="content">{item.content} </p>
                         </div>
-                    <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
                     </div>
                 </SwiperSlide>
                 ))}
