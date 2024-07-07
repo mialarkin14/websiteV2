c744e6125c9a5daaf4186eac6150bb6c9dd11046
Mia Larkin <mia.larkin1@icloud.com>
2024-07-06 21:36:47 -0400

    update swiper content

--- src/assets/swiperContent.tsx
+++ src/assets/swiperContent.tsx
@@ -1,44 +1,44 @@
 import c from "./skills/c.png";
 import css from "./skills/css.png";
 import html from "./skills/html.png";
-import java from "./skills/java.png";
+import processing from "./skills/processing.png";
 import python from "./skills/python.png";
 import react from "./skills/react.png"
 import riscv from "./skills/riscv.png"
   export const SwiperImages = [
     {
       title: "python",
-      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      content:"Advanced | 3 years Experience | Learned Data Structures and OOP.",
       Image: python,
     },
     {
       title: "c",
-      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+      content: "Intermediate | 1 year Experience | Learned Systems Programming.",
       Image: c,
     },
     {
       title: "risc-v",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      content: "Beginner | 1 year Experience | Learned 32-Bit Risc-V Assembly.",
       Image: riscv,
     },
     {
-      title: "java",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      Image: java,
+      title: "processing",
+      content: "Intermediate | 1 year Experience | Learned Programming Fundamentals",
+      Image: processing,
     },
     {
       title: "css",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      content: "Intermediate | 1 year Experience | Learned for styling HTML",
       Image: css,
     },
     {
       title: "html",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      content: "Intermediate | 1 year Experience | Learned for website creation",
       Image: html,
     },
     {
       title: "react",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      content: "Beginner | 1 year Experience | Learned Typescript and JavaScript",
       Image: react,
     },
 
