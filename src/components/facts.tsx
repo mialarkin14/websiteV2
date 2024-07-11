bc6cf68a91159095335e43eaff8950ce49948c10
Mia Larkin <mia.larkin1@icloud.com>
2024-07-11 18:46:00 -0400

    instagram

--- src/components/facts.tsx
+++ src/components/facts.tsx
@@ -1,21 +1,35 @@
+import { InstagramEmbed } from 'react-social-media-embed';
+
 const Facts = () =>{
     return(
         <>
-        <div className="Facts" style={{display:'flex', flexDirection:'row', flexWrap:'wrap', 
-            justifyContent:'center', alignItems:'center', padding:'150px', columnGap:'200px', rowGap:'50px'}}>
-                <div>
-                    <img src="src/assets/hang.png" alt="hang" className="hang"/>
+        <div className="Facts" style={{paddingBottom:'150px', boxShadow: '10px 15px 20px #09c6ff'}}>
+            <div className="RandomFacts" style={{display:'flex', flexDirection:'row', flexWrap:'wrap', 
+                justifyContent:'center', alignItems:'center', padding:'150px', columnGap:'200px', rowGap:'50px'}}>
+                    <div>
+                        <img src="src/assets/hang.png" alt="hang" className="hang"/>
+                    </div>
+                    <div>
+                        <p className="TextTitle" style={{paddingBottom:'50px'}}>random facts:</p>
+                        <ul >
+                            <li style={{color:'white', width:'400px'}}><p className="SubHeading2">i'm a big foodie and love spicy food</p></li>
+                            <li style={{color:'white', width:'400px'}}><p className="SubHeading2">i've played violin since i was 7 years old</p></li>
+                            <li style={{color:'white', width:'400px'}}><p className="SubHeading2">my favorite chips are monster munch from my childhood in ireland</p></li>
+                            <li style={{color:'white', width:'400px'}}><p className="SubHeading2">i've never broken a bone</p></li>
+                        </ul>
+                    </div>
+            </div>
+            <div className="instagram">
+                <p className="SubHeading1" style={{marginRight:'150px', marginLeft:'150px'}}>
+                    i love taking pictures of nature and the food i eat! you can find my canvas of drawings, landscapes, food and more 
+                    on my instagram 
+                </p>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <InstagramEmbed url="https://www.instagram.com/shadowillusion14/" width={500} style={{marginTop:'100px', borderRadius: '20px', boxShadow: '0 0 40px 15px #09c6ff'}} />
                 </div>
-                <div>
-                    <p className="TextTitle" style={{paddingBottom:'50px'}}>random facts:</p>
-                    <ul >
-                        <li style={{color:'white', width:'400px'}}><p className="SubHeading2">i'm a big foodie and love spicy food</p></li>
-                        <li style={{color:'white', width:'400px'}}><p className="SubHeading2">i've played violin since i was 7 years old</p></li>
-                        <li style={{color:'white', width:'400px'}}><p className="SubHeading2">my favorite chips are monster munch from my childhood in ireland</p></li>
-                        <li style={{color:'white', width:'400px'}}><p className="SubHeading2">i've never broken a bone</p></li>
-                    </ul>
-                </div>
+            </div>
         </div>
+        
         </>
     )
 }
