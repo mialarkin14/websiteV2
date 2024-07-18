c003f4d14b9b191f730c55ea7efa6490d4828a08
Mia Larkin <mia.larkin1@icloud.com>
2024-07-18 19:37:21 -0400

    url update

--- src/components/moreProjects.tsx
+++ src/components/moreProjects.tsx
@@ -4,7 +4,7 @@ const MoreProjects = () =>{
             <div style={{display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'row', 
                 boxShadow: '10px 15px 20px #09c6ff', paddingBottom:'150px', paddingTop:'150px', columnGap:'150px', rowGap:'50px'}}>
                 <p className="TextTitle" style={{width: '300px'}}>
-                    check out my other projects on my <a href="https://github.com/mialarkin14">github</a>
+                    check out my other projects on my <a href="https://github.com/mialarkin14" target="_blank" >github</a>
                 </p>
                 <a href="https://github.com/mialarkin14" target="_blank" ><img src="src/assets/navbar/git.png" alt="github" 
                 className="git"/></a>
